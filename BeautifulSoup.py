d03a978dcf25831e9358d9103201e1ea8a755937
sohee <totns6543@naver.com>
2020-10-16 12:51:54 +0900

    파일정리

--- BeautifulSoup.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen("http://www.naver.com")
-bsObject = BeautifulSoup(html, "html.parser")
-
-print(bsObject.head.title)
