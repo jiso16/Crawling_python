6033bfbde3b9e09417e2f1c55ff8577ddd5f87b9
sohee <totns6543@naver.com>
2020-10-27 15:52:08 +0900

    크롤링 - DB연동 완성

--- ShoppingMall/database.py
+++ ShoppingMall/database.py
@@ -1,14 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import re
-import ssl
-import datetime
 import pymysql
-#context = ssl._create_unverified_context()
 
 url = "https://smartstore.naver.com/mojjimoddi/category/ALL?cp=1"
-
 
 conn = pymysql.connect(host='localhost', user='root', password='wlthgml97', db='crawling',charset = 'utf8')
 
@@ -33,14 +27,12 @@ for item in items:
     price = item.find("span",attrs={"class":"nIAdxeTzhx"}).get_text()
     link = "https://smatstore.naver.com" + item.a["href"]
     review = item.find("div",attrs={"class":"_27Y22p2kob"})
-    #review = item.find("em",attrs={"class":"_1dH1kEDaAZ"})
     if review:
         review = review.get_text()
     else:
         review = "no rivew, no star"
 
     store(title,price , review,link)
-#result = curs.fetchall
 
 curs.close()
 conn.close()
